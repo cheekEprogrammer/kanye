9a31581adaec5fc73ce09267bfc53fc3cb7869ae
Cheekyprogrammer <cheekyprogrammer@gmail.com>
2022-05-25 19:26:30 +0000

    made picture larger to 40%

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", function(req, res){
     response.on("data", function(data){
       const quote = JSON.parse(data);
 
-      res.write("<title>its kanye time</title><div style='padding-left: 25%;padding-right: 25%;margin-top: 10%;text-align: center;'<br><img src='https://www.pngkey.com/png/full/163-1636867_kanye-west-png-transparent-kanye-west-face-transparent.png' width='30%'>");
+      res.write("<title>its kanye time</title><div style='padding-left: 25%;padding-right: 25%;margin-top: 10%;text-align: center;'<br><img src='https://www.pngkey.com/png/full/163-1636867_kanye-west-png-transparent-kanye-west-face-transparent.png' width='40%'>");
       res.write("<h2><i>" + quote.quote + "</i></h2></div>");
       res.send();
     });
